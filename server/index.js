f8967e3bec3067d5f997175b1031bb4b0133fa94
Thong <lminhthong1@gmail.com>
2021-08-24 10:35:50 +0700

    [Authentication] Login, register

--- server/index.js
+++ server/index.js
@@ -1,14 +1,37 @@
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-const myPlaintextPassword = "s0//P4$$w0rD";
-const someOtherPlaintextPassword = "not_bacon";
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRouter = require("./routers/auth")
+const dotenv = require("dotenv");
 
-bcrypt.genSalt(saltRounds, function (err, salt) {
-  bcrypt.hash(someOtherPlaintextPassword, salt, function (err, hash) {
-    console.log(hash);
-    bcrypt.compare(someOtherPlaintextPassword, hash, function(err, result) {
-        console.log(result);
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const URI = process.env.DATABASE;
+
+app.use(express.json());
+//app.use(express.urlencoded({ extended: true, limit: "30mb" }));
+app.use(cors());
+
+app.use("/test", authRouter);
+
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
+  })
+  .catch((err) => {
+    console.log("err", err);
   });
-});
+
 
---
+++ server/models/UserModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Users", UserSchema);
